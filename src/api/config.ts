feb239562abee9a434a4ae18c54fab8585040c7c
Quoc Duong <caodangquocduong2000ht@gmail.com>
2023-08-14 09:43:19 +0700

    update

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,4 +1,7 @@
-const PORT = 3000;
-export const API_URL = `http://localhost:${PORT}/api`;
-export const IMAGE_URL = `http://localhost:${PORT}`;
-export const BOOK_IMG_URL = `http://localhost:${PORT}/img/books`;
+// const PORT = 3000;
+// export const API_URL = `http://localhost:${PORT}/api`;
+export const API_URL = `https://api-bookstore-1cpj.onrender.com/api`;
+// export const IMAGE_URL = `http://localhost:${PORT}`;
+export const IMAGE_URL = `https://api-bookstore-1cpj.onrender.com`;
+// export const BOOK_IMG_URL = `http://localhost:${PORT}/img/books`;
+export const BOOK_IMG_URL = `https://api-bookstore-1cpj.onrender.com/img/books`;
